9ea82108517e943d29ad62dff71143e014092237
RafaelRibeiro398 <klkp.398@gmail.com>
2026-04-22 21:46:28 -0300

    Implementado Plugin PWA - Uso offiline por aplicativo do site

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      devOptions: {
+        enabled: true, // só pra testar em dev (opcional)
+      },
+
+      manifest: {
+        name: 'Meu App',
+        short_name: 'App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+
+      workbox: {
+        navigateFallback: '/index.html'
+      }
+    })
+  ],
+  server: {
+    allowedHosts: ['wimp-thus-remnant.ngrok-free.dev'] 
+  }
 })
