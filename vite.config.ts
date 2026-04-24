60c30cef77fc96859d22754912eafc144f770fd6
Kauan Machado <machadob.kauan@gmail.com>
2026-04-24 20:17:00 -0300

    fonte funcionando no PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,49 +5,74 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    VitePWA({
-      registerType: 'autoUpdate',
+  VitePWA({
+    registerType: 'autoUpdate',
 
-      devOptions: {
-        enabled: true, // só pra testar em dev (opcional)
-      },
+    includeAssets: [
+      '**/*.png',
+      '**/*.jpg',
+      '**/*.jpeg',
+      '**/*.svg',
+      '**/*.webp',
+      '**/*.woff',
+      '**/*.woff2',
+      '**/*.ttf',
+      '**/*.otf',
+      '**/*.eot'
+    ],
 
-      manifest: {
-        name: 'Juqueriquere - App de Trilhas',
-        short_name: 'Juqueriquere - Trilhas',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#008A66',
-        theme_color: '#008A66',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
+    devOptions: {
+      enabled: true, // só pra testar em dev (opcional)
+    },
+
+    manifest: {
+      name: 'Juqueriquere - App de Trilhas',
+      short_name: 'Juqueriquere - Trilhas',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#008A66',
+      theme_color: '#008A66',
+      icons: [
+        {
+          src: '/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+
+    workbox: {
+      navigateFallback: '/index.html',
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) => request.mode === 'navigate',
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'pages-cache'
           }
-        ]
-      },
-
-      workbox: {
-        navigateFallback: '/index.html',
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.mode === 'navigate',
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'pages-cache'
+        },
+        {
+          urlPattern: ({ request }) => request.destination === 'image',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images-cache',
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24 * 30
             }
           }
-        ]
-      }
-    })
+        }
+      ]
+    }
+
+  })
   ],
   server: {
-    allowedHosts: ['wimp-thus-remnant.ngrok-free.dev'] 
+    allowedHosts: ['wimp-thus-remnant.ngrok-free.dev']
   }
 })
