37d1f4360075c98d3d647f886f2e06c745bc1f36
FatalRestart <fatalrestart1.gmail.com>
2026-04-23 21:37:02 -0300

    Handle typos on SimpleButton

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Trilha from './pages/Trilhas/Trilha.tsx';
 import NotFound from './pages/NotFound.tsx';
 import Header from './components/ui/Header.tsx';
 import Trilhas from "./pages/Trilhas/Trilhas.tsx";
+import Footer from "./components/ui/Footer.tsx";
 
-import Footer from "./components/ui/footer.tsx";
 const HomePage = () => {
   return (
     <>
@@ -31,9 +31,10 @@ export default function App(){
           <Route path="/trilha/:id" element={<Trilha />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
+        <div id='reader'></div>
         <Footer />
       </Router>
-      <div id='reader'></div>
+      
     </>
   )
 };
--- src/components/ui/buttons/simpleButton.tsx
+++ src/components/ui/buttons/simpleButton.tsx
@@ -2,8 +2,6 @@ import { Link } from "react-router-dom";
 import seta1 from '../../../assets/icons/seta1.png';
 import seta12 from '../../../assets/icons/seta12.png';
 
-
-
 export default function SimpleButton({path, children, tema, icon}: {path?: string, children?: any, tema?: string, icon?: string}) {
     if(!tema) tema = 'default';
     if(!icon) icon = 'seta';
@@ -16,15 +14,15 @@ export default function SimpleButton({path, children, tema, icon}: {path?: strin
         }
         
     } as any;
-
-    const imagemSrc = icons[tema]?.[icon];
+    tema = (tema && icons[tema]) ? tema : 'default';
+    const imagemSrc = icons[tema]?.[icon] || null;
     return(
             <Link to={path || ''} >
                 <div className={tema == 'dark'? 'btnDark' : ''}>
                     {children || ""} 
-                    {imagemSrc == null ?  <></> : <img src={imagemSrc} alt={icon} />}
-                   
+                    {imagemSrc && <img src={imagemSrc} alt={icon} />}
                 </div> 
             </Link>
     )
+    
 }
