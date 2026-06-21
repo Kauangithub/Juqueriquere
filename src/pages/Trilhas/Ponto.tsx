0f0457ae46777140758bddce90f14696b89f1f7c
FatalRestart <fatalrestart1@gmail.com>
2026-06-21 02:31:14 -0300

    feat(Ponto): descrição e nome cientifico

--- src/pages/Trilhas/Ponto.tsx
+++ src/pages/Trilhas/Ponto.tsx
@@ -1,7 +1,10 @@
 import { useParams, useSearchParams } from 'react-router-dom';
 import data from '../../data.json';
+
 import NotFound from '../NotFound';
+
 import SimpleButton from '../../components/ui/buttons/SimpleButton';
+import TrilhasMap from '../../components/ui/TrilhasMap';
 
 export default function Ponto() {
     const { id, nomePonto } = useParams<{ id: string; nomePonto: string }>();
@@ -28,7 +31,7 @@ export default function Ponto() {
             case 'pontos':
                 return (
                     <>
-                        <SimpleButton path={`${from}`} type='back' icon='setaBack'>Voltar para {from}</SimpleButton>
+                        <SimpleButton path={`/${from}/`} type='back' icon='setaBack'>Voltar para {from}</SimpleButton>
                         <SimpleButton path={`/trilha/${id}`} icon='none' tema='dark'>{trilha.nome}</SimpleButton>
                     </>
                 )
@@ -44,15 +47,38 @@ export default function Ponto() {
 
     return (
         <>
+
+
             <div className="paddingHeader"></div>
             
             <section className='conteudo desktopWrap'>
+               
                 <div className="vertical gap15">
-                    <div className="horizontal gap5">
+                     <div className='horizontal gap5'>
                         {goBack()}
-                    </div>
+                     </div>
+                    {ponto.latitude && ponto.longitude && (
+                        <div className="mapa">
+                            <TrilhasMap highlight={Object(ponto).nome} id={[trilha.id]} />
+                        </div>
+                    )}
+                </div>
+                <div className="vertical gap15">
                     <h1>{Object(ponto).nome}</h1>
-                    <h1>{trilha.nome}</h1>
+                    <div className='vertical gap5'>
+                        {
+                        Object(ponto).planta && (
+                            <h3>Nome científico: {Object(ponto).planta}</h3>
+                        )
+                        }
+                        <p>Presente em {trilha.nome}</p>
+                    </div>
+                    
+                    {
+                        Object(ponto).descricao && (
+                            <p>{Object(ponto).descricao}</p>
+                        )
+                    }
                     {
                         ponto.latitude && ponto.longitude && (
                             <p>Coordenadas: {ponto.latitude}, {ponto.longitude}</p>
